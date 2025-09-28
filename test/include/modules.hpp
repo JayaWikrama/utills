f65dce03640af565039d0ae546872d9e32090078
Jaya Wikrama <jayawikrama89@gmail.com>
2025-09-28 17:54:59 +0700

    feature/add: multiple file test.

---
+++ test/include/modules.hpp
@@ -0,0 +1,14 @@
+#ifndef __MODULES_HPP__
+#define __MODULES_HPP__
+
+#include "binary-tree.hpp"
+#include "debug.hpp"
+#include "queue.hpp"
+#include "string.hpp"
+#include "time.hpp"
+
+#include "doctest.h"
+
+#include <cstring>
+
+#endif
---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
+
+int main(int argc, char **argv)
+{
+    doctest::Context context;
+    int res = context.run();
+    if (context.shouldExit())
+        return res;
+    std::cout << "done with result: " << res << "!" << std::endl;
+    return res;
+}
